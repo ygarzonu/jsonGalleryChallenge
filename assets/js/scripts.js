a07f62942870bb2ab76a62590c1cc939e5f2f500
Yeiny Garzon <sakurayc@gmail.com>
2017-03-16 06:05:26 -0500

    events fixed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,24 @@
 $(document).ready(function(){
 
 	$html = "";
+	$fullimage = "";
 	$.getJSON('assets/js/gallery.json', function (data) {
 		
 		var title = data.album;
 		$("h1").show(title);
-	
-	    $.each(data.photos, function (i, f) {
+
+		$.each(data.photos, function (i, f) {
 	  		var img = "<li><img id='" + f.id + "' src='" + f.thumb_url + "' alt='" + f.title + "' /></li>";
+	    	
+	  		$fullimage = "<img id='" + f.id + "' src='" + f.image + "' alt='" + f.title + "' />";
 	    	//console.log(f);	
 	    	$("ul").append(img); //DOM -> Insert html elements
 
-	    	$html += "<li><h1>'" + f.title + "'</h1></li>'";
+	    	$html = "<li><h1>'" + f.title + "'</h1></li>'";
             $html += "'<li><p> Taken at the Intel Conference in '" + f.location + "', '" + f.date + "'</p></li>'";
             //console.log($html);
 		});
-	    
-	
+
 		$("#json-gallery").on({
 			mouseover: function(){
 				$(this).css({
@@ -40,7 +42,9 @@ $(document).ready(function(){
 				var thumbnail = $(this).attr('src');
 				//var fullimage = "<img id='" + data.photos.id + "' src='" + data.photos.image + "' alt='" + data.photos.title + "' />";
 				$("#fullimg").fadeOut(300, function(){
-					for (var i in data.photos) {
+					var fullimage
+					for (var i=0; i<data.photos.length; i++) {
+						//console.log(data.photos[i].image);
 						var fullimage = data.photos[i].image;
 						$(this).attr('src', fullimage);	
 					}
@@ -53,6 +57,10 @@ $(document).ready(function(){
 	
 
 	$('.btnPrev').click( function(){			
+		$('.mainImage').fadeOut(500).delay(300).slideDown(800);
+	});
+
+	$('.btnNext').click( function(){			
 		$('.mainImage').fadeOut(500).delay(300).slideDown(800);
 	});
 });
